f0c31acadbcbcd6bcb51e007f69bd89c754008c9
gio256 <102917377+gio256@users.noreply.github.com>
2024-02-07 16:50:12 -0700

    WildCat/pType.v: Fix induced structure instances

--- theories/WildCat/pType.v
+++ theories/WildCat/pType.v
@@ -1,108 +1,87 @@
 Require Import
   Basics.Overture
   Basics.Tactics
-  Basics.Equivalences
   Basics.PathGroupoids
   WildCat.Core
   WildCat.Displayed
   WildCat.Universe.
 
-Definition isgraph_equiv_isgraph {A B : Type} `{IsGraph B} (F : A <~> B)
+Definition isgraph_pullback_isgraph {A B : Type} `{IsGraph B} (F : A -> B)
   : IsGraph A.
 Proof.
-  apply Build_IsGraph.
+  nrapply Build_IsGraph.
   intros a b.
   exact (F a $-> F b).
 Defined.
 
-(** Defined for an arbitrary graph structure on A *)
-Definition is01cat_equiv_is01cat {A B : Type} `{IsGraph A} `{Is01Cat B}
-  (F : A <~> B) `{!Is0Functor F} `{!Is0Functor F^-1}
-  : Is01Cat A.
+Definition is01cat_pullback_is01cat {A B : Type} `{Is01Cat B} (F : A -> B)
+  : @Is01Cat A (isgraph_pullback_isgraph F).
 Proof.
-  apply Build_Is01Cat.
-  - intro a.
-    exact (transport (fun x => x $-> x) (eissect F a) (fmap F^-1 (Id (F a)))).
-  - intros a b c g f.
-    nrefine (transport (fun x => a $-> x) (eissect F c) _).
-    nrefine (transport (fun x => x $-> F^-1 (F c)) (eissect F a) _).
-    exact (fmap F^-1 (fmap F g $o fmap F f)).
-Defined.
-
-(** Defined only for our isgraph_equiv_isgraph structure on A *)
-Definition is01cat_equiv_is01cat' {A B : Type} `{Is01Cat B} (F : A <~> B)
-  : @Is01Cat A (isgraph_equiv_isgraph F).
-Proof.
-  apply Build_Is01Cat.
+  nrapply Build_Is01Cat.
   - intro a.
     exact (Id (F a)).
-  - intros a b c g f.
-    exact (cat_comp (A:=B) g f).
+  - intros a b c.
+    exact (cat_comp (A:=B)).
 Defined.
 
-Definition is2graph_equiv_is2graph {A B : Type} `{IsGraph A} `{Is2Graph B}
-  (F : A <~> B) `{!Is0Functor F}
-  : Is2Graph A.
+Definition is2graph_pullback_is2graph {A B : Type} `{Is2Graph B} (F : A -> B)
+  : @Is2Graph A (isgraph_pullback_isgraph F).
 Proof.
   intros a b.
-  apply Build_IsGraph.
-  intros f g.
-  exact (fmap F f $-> fmap F g).
+  exact (isgraph_hom (F a) (F b)).
 Defined.
 
-Definition is2graph_equiv_is2graph' {A B : Type} `{Is2Graph B} (F : A <~> B)
-  : @Is2Graph A (isgraph_equiv_isgraph F).
+Definition is0gpd_pullback_is0gpd {A B : Type} `{Is0Gpd B} (F : A -> B)
+  : @Is0Gpd A _ (is01cat_pullback_is01cat F).
 Proof.
-  intros a b.
-  apply Build_IsGraph.
-  intros f g.
-  exact (@Hom (@Hom B _ _ _) _ f g).
-Defined.
-
-Definition is0gpd_equiv_is0gpd {A B : Type} `{IsGraph A, !Is01Cat A} 
-  `{Is0Gpd B} (F : A <~> B) `{!Is0Functor F} `{!Is0Functor F^-1}
-  : Is0Gpd A.
-Proof.
-  srapply Build_Is0Gpd.
-  intros a b f.
-  nrefine (transport (fun x => b $-> x) (eissect F a) _).
-  nrefine (transport (fun x => x $-> F^-1 (F a)) (eissect F b) _).
-  exact (fmap F^-1 (gpd_rev (fmap F f))).
-Defined.
-
-Definition is0gpd_equiv_is0gpd' {A B : Type} `{Is0Gpd B} (F : A <~> B) 
-  : @Is0Gpd A _ (is01cat_equiv_is01cat' F).
-Proof.
-  srapply Build_Is0Gpd.
+  nrapply Build_Is0Gpd.
   intros a b f.
   exact (gpd_rev (A:=B) f).
 Defined.
 
+Definition is1cat_pullback_is1cat {A B : Type} `{ic : Is1Cat B} (F : A -> B)
+  : @Is1Cat A _ (is2graph_pullback_is2graph F) (is01cat_pullback_is01cat F).
+Proof.
+  snrapply Build_Is1Cat.
+  - intros a b.
+    exact (is01cat_hom (F a) (F b)).
+  - intros a b.
+    exact (is0gpd_hom (F a) (F b)).
+  - intros a b c.
+    exact (is0functor_postcomp (F a) (F b) (F c)).
+  - intros a b c.
+    exact (is0functor_precomp (F a) (F b) (F c)).
+  - intros a b c d.
+    exact (cat_assoc (A:=B)).
+  - intros a b.
+    exact (cat_idl (A:=B)).
+  - intros a b.
+    exact (cat_idr (A:=B)).
+Defined.
 
-Definition is_pointed (A : Type) := IsPointed A.
 
-Local Instance isdgraph_pointed : IsDGraph is_pointed.
+Local Instance isdgraph_pointed : IsDGraph IsPointed.
 Proof.
   intros A B f a b.
   exact (f a = b).
 Defined.
 
-Local Instance is01dcat_pointed : Is01DCat is_pointed.
+Local Instance is01dcat_pointed : Is01DCat IsPointed.
 Proof.
   apply Build_Is01DCat.
   - intros A a.
-    reflexivity.
+    exact idpath.
   - intros A B C g f a b c pg pf.
     exact (ap g pf @ pg).
 Defined.
 
-Local Instance is2dgraph_pointed : Is2DGraph is_pointed.
+Local Instance is2dgraph_pointed : Is2DGraph IsPointed.
 Proof.
   intros A B a b f g H pf pg.
   exact (H a @ pg = pf).
 Defined.
 
-Local Instance is1dcat_pointed : Is1DCat is_pointed.
+Local Instance is1dcat_pointed : Is1DCat IsPointed.
 Proof.
   snrapply Build_Is1DCat.
   - intros A B a b.
@@ -139,22 +118,17 @@ Definition issig_ptype : { X : Type & X } <~> pType := ltac:(issig).
 
 Local Instance isgraph_ptype : IsGraph pType.
 Proof.
-  apply (isgraph_equiv_isgraph issig_ptype).
+  exact (isgraph_pullback_isgraph issig_ptype^-1).
 Defined.
 
 Local Instance is01cat_ptype : Is01Cat pType.
 Proof.
-  nrapply (is01cat_equiv_is01cat issig_ptype).
-  - exact (is01cat_sigma is_pointed).
-  - by apply Build_Is0Functor.
-  - apply Build_Is0Functor.
-    intros Aa Bb f; apply f.
+  nrapply (is01cat_pullback_is01cat issig_ptype^-1).
+  - exact (is01cat_sigma IsPointed).
 Defined.
 
 Local Instance is2graph_ptype : Is2Graph pType.
 Proof.
-  nrapply (is2graph_equiv_is2graph issig_ptype).
-  - apply (is2graph_sigma is_pointed).
-  - apply Build_Is0Functor.
-    intros Aa Bb f; apply f.
+  nrapply (is2graph_pullback_is2graph issig_ptype^-1).
+  - exact (is2graph_sigma IsPointed).
 Defined.
